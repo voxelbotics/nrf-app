3d3f2393ec6802c3e051aaa2c9e9b5c19c08908a
Alexander Dyachenko <sasha_d@voxelbotics.com>
2024-05-07 00:42:58 +0300

    IVE-143: Add configuration files with app version

--- thingy9151lite_nrf9151_app/src/main.c
+++ thingy9151lite_nrf9151_app/src/main.c
@@ -4,12 +4,18 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include "config.h"
 
 LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
-	LOG_INF("Start thingy9151lite_nrf9151_app on %s", CONFIG_BOARD);
+	LOG_INF("Start thingy9151lite_nrf9151_app v%d.%d-%d-%s on %s",
+		CONFIG_VERSION_MAJOR,
+		CONFIG_VERSION_MINOR,
+		CONFIG_BUILD_NUMBER,
+		CONFIG_BUILD_DEBUG ? "debug" : "release",
+		CONFIG_BOARD);
 
 	return 0;
 }
