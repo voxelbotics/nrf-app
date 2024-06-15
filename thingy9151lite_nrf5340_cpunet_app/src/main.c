30a44d2bc62c206dbdba04576357a02633ff2d4e
Alexander Dyachenko <sasha_d@voxelbotics.com>
2024-06-16 00:37:47 +0300

    IVE-179: Merge the old nrf5340 cpunet app into nRF RPC Bluetooth Host

--- thingy9151lite_nrf5340_cpunet_app/src/main.c
+++ thingy9151lite_nrf5340_cpunet_app/src/main.c
@@ -5,9 +5,17 @@
  */
 
 #include <zephyr/sys/printk.h>
+#include "config.h"
 
 int main(void)
 {
+	printk("Start thingy9151lite_nrf5340_cpunet_app v%d.%d-%d-%s on %s\n",
+	       CONFIG_VERSION_MAJOR,
+	       CONFIG_VERSION_MINOR,
+	       CONFIG_BUILD_NUMBER,
+	       CONFIG_BUILD_DEBUG ? "debug" : "release",
+	       CONFIG_BOARD);
+
 	printk("Starting nRF RPC bluetooth host\n");
 
 	return 0;
