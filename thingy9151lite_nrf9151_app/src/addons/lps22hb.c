48deb4d0f335bc7420687f76924e7cdb0fed22fd
Dmitry Alexeev <dimaa@voxelbotics.com>
2024-06-19 19:52:27 +0300

    IVE-161: Set low noise mode by default for LPS22HB and LPS22HH

--- thingy9151lite_nrf9151_app/src/addons/lps22hb.c
+++ thingy9151lite_nrf9151_app/src/addons/lps22hb.c
@@ -1,3 +1,12 @@
+/*
+ * LPS22HB init
+ *
+ * Copyright (c) 2024 Emcraft Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/sensor.h>
 
@@ -26,10 +35,24 @@ void lps22hb_init()
 {
 	if (IS_ENABLED(CONFIG_LPS22HB_TRIGGER)) {
 		const struct device *const dev = DEVICE_DT_GET_ONE(st_lps22hb_press);
-		struct sensor_value attr;
+		int ret;
+
+		/* Set ODR to 10 Hz */
+		struct sensor_value attr = {
+			.val1 = 10,
+			.val2 = 0,
+		};
+
+		/* Set trigger */
 		struct sensor_trigger trig = {
 			.type = SENSOR_TRIG_DATA_READY,
 			.chan = SENSOR_CHAN_ALL,
+		};
+
+		/* Set low noise mode */
+		struct sensor_value pm_attr = {
+			.val1 = 1,
+			.val2 = 0,
 		};
 
 		if (!device_is_ready(dev)) {		
@@ -38,12 +61,21 @@ void lps22hb_init()
 
 		if (sensor_trigger_set(dev, &trig, lps22hb_handler) < 0) {
 			LOG_ERR("Cannot configure trigger");
+			return;
 		}
 
-		attr.val1 = 10;
-		attr.val2 = 0;
+		ret = sensor_attr_set(dev, SENSOR_CHAN_ALL, SENSOR_ATTR_SAMPLING_FREQUENCY, &attr);
 
-		sensor_attr_set(dev, SENSOR_CHAN_ALL, SENSOR_ATTR_SAMPLING_FREQUENCY, &attr);
+		if (ret != 0) {
+			LOG_ERR("Cannot set sampling frequency.\n");
+			return;
+		}
+
+		ret = sensor_attr_set(dev, SENSOR_CHAN_ALL, SENSOR_ATTR_CONFIGURATION, &pm_attr);
+
+		if (ret != 0) {
+			LOG_ERR("Cannot configure power mode.\n");
+		}
 	}
 }
 
